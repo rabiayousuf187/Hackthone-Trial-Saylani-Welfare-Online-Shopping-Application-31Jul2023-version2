f443137998bc644ef69493d1ffe23b116cc798b2
rabiayousuf187 <rabiayousuf187@gmail.com>
2023-08-03 18:21:11 +0500

    working on firebase Signup FORM-JS

---
+++ js/config/firebase-config.js
@@ -0,0 +1,19 @@
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
+// import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-analytics.js";
+
+// let firebase_settings= {
+
+    // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+let firebaseConfig = {
+    apiKey: "AIzaSyAT7yf6n4tFUjvqJKJWDcJpJM26qsQ9yOg",
+    authDomain: "hackthonetrial-saylani-shopapp.firebaseapp.com",
+    projectId: "hackthonetrial-saylani-shopapp",
+    storageBucket: "hackthonetrial-saylani-shopapp.appspot.com",
+    messagingSenderId: "450136022057",
+    appId: "1:450136022057:web:f3d1cdd876da1333949038",
+    measurementId: "G-DQZD8KB8J7"
+  };
+
+
+export {firebaseConfig};
--- js/signup.js
+++ js/signup.js
@@ -1,8 +1,33 @@
 console.log("Signup JS integrated")
 
-function signup(){
-    console.log("Form Submit!");
-    
-    
-}
+// Import the functions you need from the SDKs you need
+
+import { firebaseConfig } from "./config/firebase-config.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// let firebaseConfig = firebase_settings.firebaseConfig;
+
+console.log("import firebaseConfig === ", firebaseConfig);
+// initializeApp()
+firebase.initializeApp(firebaseConfig);
+
+    signUpForm.addEventListener("submit", (e) => {
+        console.log("Form Submit!");
+
+        e.preventDefault();
+        const email = signUpForm.email.value;
+        const password = signUpForm.password.value;
+
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+            .then((userCredential) => {
+                // User signed up successfully
+                console.log("User signed up:", userCredential.user);
+            })
+            .catch((error) => {
+                // Handle signup errors
+                console.error("Error signing up:", error);
+            });
+    });
 
