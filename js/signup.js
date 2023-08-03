71fb7178b3317edde430262fa134794404445a54
rabiayousuf187 <rabiayousuf187@gmail.com>
2023-08-04 01:40:26 +0500

    Signup Form submit option working, starting form validation

--- js/signup.js
+++ js/signup.js
@@ -1,41 +1,34 @@
 console.log("Signup JS integrated");
 
 // Config and Initialize Firebase
-import {  auth,  createUserWithEmailAndPassword,} from "./config/firebase-config.js";
-
+import {
+  auth,
+  createUserWithEmailAndPassword,
+} from "./config/firebase-config.js";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-console.log("import firebaseConfig === ", );
+console.log("import firebaseConfig === ");
 
- let signup = document.getElementById('signup');
-signup.addEventListener("submit", (e) => {
-  console.log("Form Submit!");
-  
+// Handle form submission
+let signup_form = document.getElementById("signup-form");
+signup_form.addEventListener("submit", (e) => {
   e.preventDefault();
-  let email = document.getElementById('email').value;
-  let password = document.getElementById('password').value;
-  
-    console.log('Email and Password' , email, password);
-    createUserWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-            // Signed in 
-            const user = userCredential.user;
-            // set(ref(Database , 'user/' ),{
-            //     email: email,
-            //     password: password,
-            // });
-            console.log('User Created');
-            alert('User Created');
-            // ...
-        })
-        .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
+  let email = document.getElementById("email").value;
+  let password = document.getElementById("password").value;
 
-            console.log(errorMessage ,errorCode);
-            // alert(errorMessage);
-            // ..
-        });
-}); 
+  
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      console.log("User Created");
+      alert("User Created");
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorMessage, errorCode);
+    });
+});
