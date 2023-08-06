6ba96ea1344ef277bcada36ec0eac2c65ef75459
rabiayousuf187 <rabiayousuf187@gmail.com>
2023-08-06 19:11:03 +0500

    working on auth and logout fun

--- js/auth/auth.js
+++ js/auth/auth.js
@@ -1,3 +1,15 @@
-function isAuth(){
-    localStorage.getItem
+function isAuth(redirect){
+    localStorage.getItem('userAcc');
+    if(userAcc!==null){
+        window.location.href = redirect;
+    }
+    else{
+        window.location.href = './signin.html';    
+    }
+}
+
+function logout(){
+    localStorage.removeItem('userAcc');
+    console.log("Logout successfully! redirected to signin");
+    window.location.href = './signin.html';    
 }
