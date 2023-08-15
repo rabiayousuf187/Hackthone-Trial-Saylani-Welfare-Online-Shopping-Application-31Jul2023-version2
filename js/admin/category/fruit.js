a2941501bb226a83b86cf4bdcf593414a0ee53e6
rabiayousuf187 <rabiayousuf187@gmail.com>
2023-08-15 13:09:59 +0500

    Correct redirect Pages-Working on Fruit page

--- js/admin/category/fruit.js
+++ js/admin/category/fruit.js
@@ -1,5 +1,5 @@
-import { isAuth } from "../auth/auth.js";
-import firebaseExports from "../config/firebase-config.js";
+import { isAuth } from "../../auth/auth.js";
+import firebaseExports from "../../config/firebase-config.js";
 let userAcc = isAuth();
 console.log("userAcc get via is Auth()", userAcc);
 
@@ -25,23 +25,27 @@ if (userAcc && userAcc.acc_type === "admin") {
     console.log("current_page color change", current_page);
     current_page.querySelector('img').style.filter = 'invert(62%) sepia(112%) saturate(349%) hue-rotate(61deg) brightness(56%) contrast(168%)';
     current_page.querySelector('p').style.color = "#61B846";
+    current_page.addEventListener('click', function (event) {
+        event.preventDefault(); // Prevent the default link behavior
+        window.location.href = "../admin.html"
+    });
 
     let add_item = document.getElementById("add-item");
     add_item.addEventListener('click', function (event) {
         event.preventDefault(); // Prevent the default link behavior
-        window.location.href = "./add-item.html"
+        window.location.href = "../add-item.html"
     });
 
     let acc_setting = document.getElementById("acc-setting");
     acc_setting.addEventListener('click', function (event) {
         event.preventDefault(); // Prevent the default link behavior
-        window.location.href = "./account-setting.html"
+        window.location.href = "../account-setting.html"
     });
 
     let order = document.getElementById("order");
     order.addEventListener('click', function (event) {
         event.preventDefault(); // Prevent the default link behavior
-        window.location.href = "./order.html"
+        window.location.href = "../order.html"
     });
 
 } else if (
