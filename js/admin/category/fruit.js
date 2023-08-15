fceb021067a33e93bd9d29baa41dc6763d42dd99
rabiayousuf187 <rabiayousuf187@gmail.com>
2023-08-15 14:47:21 +0500

    Successfully Get Data of Sub Category-Fruit page

--- js/admin/category/fruit.js
+++ js/admin/category/fruit.js
@@ -4,21 +4,19 @@ let userAcc = isAuth();
 console.log("userAcc get via is Auth()", userAcc);
 
 if (userAcc && userAcc.acc_type === "admin") {
+    let itemsData;
     console.log("Admin-Home: Fruit Page");
 
     document.getElementById("Top").style.display = "block";
     // Use the Firebase Configuration functions
     const {
-        auth,
-        createUserWithEmailAndPassword,
         database,
         ref,
-        set,
+        get,
         storage,
         storageRef,
-        uploadBytes,
-        getDownloadURL,
     } = firebaseExports;
+
 
 
     let current_page = document.getElementById("home");
@@ -47,42 +45,91 @@ if (userAcc && userAcc.acc_type === "admin") {
         event.preventDefault(); // Prevent the default link behavior
         window.location.href = "../order.html"
     });
+let  getImg = async (itemCategory , itemName ) => {
+    try {
+        const imageUrl = await storageRef(storage,`images/${itemCategory}/${itemName}`
+          ).getDownloadURL();
+        return imageUrl;
+    } catch (error) {
+        console.error("Error getting image from storage:", error);
+        return null;
+    }
 
-    let getAllItemData = () => {
-        return new Promise((resolve, reject) => {
-            // get reference to the Firebase Realtime Database
-            // get data to the database
-            set(ref(database, `items/fruit/`), {
-              itemName: itemname,
-              itemCategory: selectedCategory,
-              itemContent: itemcontent,
-              unitName: unitname,
-              unitPrice: unitprice,
-              imageUrl: downloadURL, // Store the image URL here
-            })
-            dataRef.once('value')
-        .then((snapshot) => {
-            // snapshot contains the data
-            const data = snapshot.val();
+}
+// Create a function to generate and add a fruit item to the container
+let addFruitItem = (name, weight, price, imageURL) => {
+    const container = document.getElementById("fruit-container");
 
-            // Process the data as needed
-            console.log('Retrieved data:', data);
+    const div = document.createElement("div");
+    div.classList.add("cat-1");
+
+    const button = document.createElement("button");
+    button.classList.add("col-12", "btn", "btn-get-started", "cat-inp");
+
+    const fruitTitleDiv = document.createElement("div");
+    fruitTitleDiv.classList.add("fruit-title");
+
+    const img = document.createElement("img");
+    img.src = imageURL;
+    img.alt = name;
+
+    const nameDiv = document.createElement("div");
+
+    const nameParagraph = document.createElement("p");
+    nameParagraph.id = "fruit-name";
+    nameParagraph.textContent = name;
+
+    const weightParagraph = document.createElement("p");
+    weightParagraph.id = "fruit-weight";
+    weightParagraph.textContent = weight;
+
+    const priceParagraph = document.createElement("p");
+    priceParagraph.id = "fruit-price";
+    priceParagraph.textContent = price;
+
+    // Append elements in the correct hierarchy
+    container.appendChild(div);
+    div.appendChild(button);
+    button.appendChild(fruitTitleDiv);
+    fruitTitleDiv.appendChild(img);
+    fruitTitleDiv.appendChild(nameDiv);
+    nameDiv.appendChild(nameParagraph);
+    nameDiv.appendChild(weightParagraph);
+    button.appendChild(priceParagraph);
+}
+
+    let getAllItemData = async () => {
+        try {
+            const snapshot = await get(ref(database, `items/fruit/`));
+            // Data snapshot contains the data at the specified location
+            itemsData = snapshot.val();
+            return itemsData;
+        } catch (error) {
+            console.error("Error getting data:", error);
+            return false;
+        }
+    }
+
+    getAllItemData()
+        .then((data) => {
+            if (!data) {
+                console.log("Data is null")
+            } else {
+                console.log('Retrieved data:', data);
+                data.map((ele) => {
+                    
+                    // Call the function to add a fruit item
+                    // name, weight, price, imageURL
+                    addFruitItem(ele.itemName,ele.unitName,ele.unitPrice,ele.imageUrl);
+
+                });
+            }
+            // Process the retrieved data
+
         })
         .catch((error) => {
             console.error('Error fetching data:', error);
         });
-
-              .then(() => {
-                console.log("Data saved to Firebase Database. with Img");
-                resolve(); // Resolve the promise to indicate success
-              })
-              .catch((error) => {
-                console.error("Error saving data:", error);
-                reject(error); // Reject the promise with the error
-              });
-          });
-    }
-
 } else if (
     (userAcc && userAcc.acc_type === "user") ||
     userAcc === null ||
