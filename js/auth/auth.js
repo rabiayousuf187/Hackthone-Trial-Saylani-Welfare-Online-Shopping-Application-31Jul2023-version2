39423af084d5a00915ce51d78692e7d3d206a0e4
rabiayousuf187 <rabiayousuf187@gmail.com>
2023-08-17 13:23:20 +0500

    remove whole selected origin local storage

--- js/auth/auth.js
+++ js/auth/auth.js
@@ -13,7 +13,20 @@ function isAuth() {
   }
 }
 function logout() {
-  localStorage.removeItem("userAcc");
+  const currentOrigin = window.location.origin;
+  console.log("Current origin:", currentOrigin);
+  
+  for (let key in localStorage) {
+    if (localStorage.hasOwnProperty(key)) {
+      console.log("localStorage.removeItem ===== ", key);
+      localStorage.removeItem(key);
+    }
+  }
+
+console.log("Local storage data for the origin has been removed.");
+
+  // localStorage.removeItem("userAcc");
+  // localStorage.removeItem
   console.log("Logout successfully! redirected to signin");
   window.location.href = "../auth/signin.html";
 }
