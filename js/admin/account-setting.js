13b8e2fd7a2a128444d2a8da491deec6e054ca51
rabiayousuf187 <rabiayousuf187@gmail.com>
2023-08-17 19:34:47 +0500

    Start Working on Account Setting Page

--- js/admin/account-setting.js
+++ js/admin/account-setting.js
@@ -1,47 +1,47 @@
-import { isAuth, logout } from "../auth/auth.js";
-
+import { isAuth } from "../auth/auth.js";
 import firebaseExports from "../config/firebase-config.js";
-
-const { auth, signInWithEmailAndPassword, database, ref, get } = firebaseExports;
-// //Only For Auth User: Config and Initialize Firebase
-// if (firebaseExports !== null) {
-//     // Use the Firebase functions
-//     // Now you can use these imported functions
-//     // ...
-// } else {
-//     console.log("User Already Logged In, didnot required Firebase");
-//     // window.location.href = './signin.html';
-// }
-
 let userAcc = isAuth();
 console.log("userAcc get via is Auth()", userAcc);
 
-if (userAcc && userAcc.acc_type === 'admin') {
-    console.log("Account Setting Page");
+if (userAcc && userAcc.acc_type === "admin") {
+  console.log("Admin Account Setting Page");
+
+  document.getElementById("Top").style.display = "block";
+//   document.getElementById("adminname").innerText = userAcc.fullname;
+  // Use the Firebase Configuration functions
+  const {
+    database,
+    ref,
+    set,
+    storage,
+    storageRef,
+    uploadBytes,
+    getDownloadURL,
+  } = firebaseExports;
+
+  const addClickListener = (elementId, destination) => {
+    const element = document.getElementById(elementId);
+    element.addEventListener("click", (event) => {
+      event.preventDefault();
+      window.location.href = destination;
+    });
+  };
 
     let current_page = document.getElementById("acc-setting");
     console.log("current_page color change", current_page);
     current_page.querySelector('img').style.filter = 'invert(62%) sepia(112%) saturate(349%) hue-rotate(61deg) brightness(56%) contrast(168%)';
     current_page.querySelector('p').style.color = "#61B846";
 
-    let home = document.getElementById("home");
-    home.addEventListener('click', function (event) {
-        event.preventDefault(); // Prevent the default link behavior
-        window.location.href = "./sale-product.html"
-    });
+    addClickListener('home' , './admin.html');
+    addClickListener('add-item' , './add-item.html');
 
-    let add_item = document.getElementById("add-item");
-    add_item.addEventListener('click', function (event) {
-        event.preventDefault(); // Prevent the default link behavior
-        window.location.href = "./add-item.html"
-    });
-
+    
     var logoutbtn = document.getElementById('logout');
 
     logoutbtn.addEventListener('click', function () {
         console.log("Logout");
         setTimeout( ()=>{
             logout();
-        } , 3000);
+        } , 1000);
     });
 }
