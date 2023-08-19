b65a7b393b921f964d2d578a38da0f3f5bc221fb
rabiayousuf187 <rabiayousuf187@gmail.com>
2023-08-19 20:16:54 +0500

    update footer links

--- js/purchase/account-setting.js
+++ js/purchase/account-setting.js
@@ -290,7 +290,7 @@ if (userAcc && userAcc.acc_type === "user") {
     }
   };
 
-let current_page  = document.getElementById("order");
+let current_page  = document.getElementById("acc-setting");
 console.log("current_page color change", current_page);
 current_page.querySelector('img').style.filter = 'invert(62%) sepia(112%) saturate(349%) hue-rotate(61deg) brightness(56%) contrast(168%)';
 current_page.querySelector('img').style.background = '#2b75142b';
@@ -299,20 +299,15 @@ current_page.querySelector('img').style.borderRadius = '4px';
 let home  = document.getElementById("home");
 home.addEventListener('click', function(event) {
     event.preventDefault(); // Prevent the default link behavior
-    window.location.href = "./sale-product.html"
+    window.location.href = "./purchase.html"
 });
 
-let add_item  = document.getElementById("add-item");
+let add_item  = document.getElementById("cart");
 add_item.addEventListener('click', function(event) {
     event.preventDefault(); // Prevent the default link behavior
-    window.location.href = "./add-item.html"
+    window.location.href = "./cart.html"
 });
 
-let cart  = document.getElementById("cart");
-acc_setting.addEventListener('click', function(event) {
-    event.preventDefault(); // Prevent the default link behavior
-    window.location.href = "./account-setting.html"
-});
 
     var logoutbtn = document.getElementById("logout");
 
